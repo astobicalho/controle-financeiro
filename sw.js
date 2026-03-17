f743a33aae7591688d227eab7269057957ba6048
astobicalho <astobicalho@gmail.com>
2026-03-17 17:38:12 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+// Service Worker para permitir a instalação
+const CACHE_NAME = 'financas-up-v1';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
